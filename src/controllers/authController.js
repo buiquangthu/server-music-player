bd46845943f15d7deb89442e1dbdd84d4c038607
buiquangthu <bqt05122002@gmail.com>
2024-07-08 16:01:36 +0700

    add handler register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,26 +2,81 @@ const UserModel = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+        user: process.env.USERNAME_EMAIL,
+        pass: process.env.PASSWORD_EMAIL,
+    },
+});
+
+
+const handlerSendEmail = async (val, email) => {
+
+    try {
+        await transporter.sendMail({
+            from: `"Support MusicPlayer" <${process.env.USERNAME_EMAIL}>`, // sender address
+            to: email, // list of receivers
+            subject: "Xác minh email", // Subject line
+            text: "your code to verification email", // plain text body
+            html: `<h1>${val}</h1>`, // html body
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const verification = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+
+    const verificationCode = Math.round(1000 + Math.random() * 9000);
+    console.log(verificationCode)
+
+    try {
+        await handlerSendEmail(verificationCode, email);
+        res.status(200).json({
+            message: 'Gửi Email thành công',
+            data: {
+                code: verificationCode
+            }
+        })
+    } catch (error) {
+        res.status(401)
+        throw new Error('Không thể gửi Email')
+    }
+})
+
+
+
+const getJsonWebToken = async (email, id) => {
+    const payload = {
+        email,
+        id,
+    };
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+        expiresIn: '1h',
+    });
+    return token;
+}
+
 const register = asyncHandler(async (req, res) => {
 
     const { userName, email, passWord } = req.body;
     const checkEmail = await UserModel.findOne({ email });
 
-    const getJsonWebToken = async (email, id) => {
-        const payload = {
-            email,
-            id,
-        };
-        const token = jwt.sign(payload, process.env.SECRET_KEY, {
-            expiresIn: '1h',
-        });
-        return token;
-    }
 
     if (checkEmail) {
 
         res.status(401)
-        throw new Error('Email already exists');
+        throw new Error('Email đã tồn tại');
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -41,10 +96,40 @@ const register = asyncHandler(async (req, res) => {
         data: {
             userName: newUser.userName,
             id: newUser.id,
-            accessToken: await  getJsonWebToken(email, newUser.id)
+            accessToken: await getJsonWebToken(email, newUser.id)
         }
     })
-    
-})
 
-module.exports = { register };
+});
+
+const login = asyncHandler(async (req, res) => {
+    const { email, passWord } = req.body;
+
+
+    // console.log(email)
+    const existingUser = await UserModel.findOne({ email });
+    if (!existingUser) {
+        res.status(403)
+        throw new Error('Người dùng không tồn tại')
+    }
+    const isMatchPassword = await bcrypt.compare(passWord, existingUser.passWord);
+    // console.log(isMatchPassword)
+    if (!isMatchPassword) {
+        res.status(403).json({
+            message: "Tài khoản hoặc mật khẩu không chính xác"
+        });
+    }
+
+    res.status(200).json({
+        message: 'Đăng nhập thành công',
+        data: {
+            id: existingUser.id,
+            email: existingUser.email,
+            accessToken: await getJsonWebToken(email, existingUser.id)
+        }
+    });
+});
+
+
+
+module.exports = { register, login, verification };
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,10 +1,12 @@
 const Router = require('express');
-const { register } = require('../controllers/authController');
+const { register, login, verification } = require('../controllers/authController');
 const errorHandlingMiddleware = require('../middlewares/errorHandlingMiddleware');
 
 const authRouter = Router();
 
 
 authRouter.post('/register',register)
+authRouter.post('/login',login)
+authRouter.post('/verification', verification)
 
 module.exports = authRouter;
